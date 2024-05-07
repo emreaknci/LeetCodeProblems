3e74460a86111e76ad3271b18e8a340c0eb9a61f
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-05-07 09:42:56 +0300

    Added Palindrome Number Problem

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿// Problem: https://leetcode.com/problems/palindrome-number/
+
+bool IsPalindrome(int x)
+{
+    string xValue = x.ToString();
+    char[] xReverseChar = new char[xValue.Length];
+
+    for (int i = 0; i < xValue.Length; i++)
+        xReverseChar[i] = xValue[xValue.Length - 1 - i];
+
+
+    string reversedString = new string(xReverseChar);
+    return xValue.Equals(reversedString);
+}
+
+
+Console.WriteLine(IsPalindrome(121));
+Console.WriteLine(IsPalindrome(-121));
