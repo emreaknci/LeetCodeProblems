b7b3f46ba51d9ac6b66d9d0393ae57a783c6122b
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-05-08 08:02:17 +0300

    Added ValidParentheses Problem

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,31 @@
+﻿// Problem: https://leetcode.com/problems/valid-parentheses/
+
+
+bool IsValid(string s)
+{
+    Stack<char> stack = new();
+    Dictionary<char, char> bracketPairs = new()
+    {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' }
+    };
+
+    foreach (char c in s)
+    {
+        if (bracketPairs.ContainsKey(c))
+            stack.Push(c);
+
+        else if (bracketPairs.ContainsValue(c))
+        {
+            if (stack.Count == 0 || bracketPairs[stack.Peek()] != c)
+                return false;
+
+            stack.Pop();
+        }
+    }
+
+    return stack.Count == 0;
+}
+
+Console.WriteLine(IsValid("()[()]{}"));
