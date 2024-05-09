28886313982e093d2e5de2133f49be25a875c1e0
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-05-09 08:15:13 +0300

    Added LengthOfLastWord Problem

---
+++ LengthOfLastWord/Program.cs
@@ -0,0 +1,15 @@
+﻿// Problem: https://leetcode.com/problems/length-of-last-word/
+int LengthOfLastWord(string s)
+{
+    s = s.Trim();
+    if (s == null) return 0;
+
+    int lastSpaceIndex=s.LastIndexOf(' ');
+    if (lastSpaceIndex == -1) return s.Length;
+
+    s=s.Substring(lastSpaceIndex,s.Length-lastSpaceIndex);
+
+    return s.Length-1;
+}
+
+Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
