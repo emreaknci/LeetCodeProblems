77b8c5080971e5750a03429d28b1294e4c6e76af
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-05-10 09:41:35 +0300

    Added Sqrt(x) Problem

---
+++ Sqrt/Program.cs
@@ -0,0 +1,15 @@
+﻿// Problem: https://leetcode.com/problems/sqrtx/description/
+int MySqrt(int num)
+{
+    if (num <= 1) return num;
+    long i = 1;
+    long square = i * i; //1
+    while (num >= square) // 8>=1 - 8>=4 - 8>=9
+    {
+        i++; // 2 - 3
+        square = i * i; // 4 - 9
+    }
+    return (int)(i - 1); // 3-1=2
+}
+
+Console.WriteLine(MySqrt(8));
