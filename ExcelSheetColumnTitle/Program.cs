e6642abcbcb38bf6431aae376fad1571f040472c
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-05-10 10:00:01 +0300

    Added ExcelSheetColumnTitle Problem

---
+++ ExcelSheetColumnTitle/Program.cs
@@ -0,0 +1,14 @@
+﻿// Problem: https://leetcode.com/problems/excel-sheet-column-title/description/
+
+using System.Text;
+
+string ConvertToTitle(int columnNumber)
+{
+    if (columnNumber <= 0)
+        return "";
+
+    int remainder = (columnNumber - 1) % 26;
+    return ConvertToTitle((columnNumber - 1) / 26) + (char)('A' + remainder);
+}
+
+Console.WriteLine(ConvertToTitle(701));
