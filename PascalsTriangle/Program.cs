d49f600957ac6146f12f218cc03fb240c03a76ac
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-05-10 08:57:00 +0300

    Added Pascal's Triangle Problem

---
+++ PascalsTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿//// Problem: https://leetcode.com/problems/pascals-triangle/description/
+
+IList<IList<int>> Generate(int numRows)
+{
+    IList<IList<int>> result = new List<IList<int>>();
+    if (numRows <= 0)
+        return result;
+
+    for (int i = 0; i < numRows; i++)
+    {
+        int[] rowNumbers = new int[i + 1];
+        rowNumbers[0] = 1; //first index must be 1
+        rowNumbers[rowNumbers.Length - 1] = 1; //last index must be 1
+
+        for (int j = 1; j < i; j++)
+        {
+            rowNumbers[j] = result[i - 1][j - 1] + result[i - 1][j]; //C(5,2) = C(4,1)+C(4,2)
+        }
+
+        result.Add(rowNumbers);
+    }
+
+    return result;
+}
+
+var result = Generate(5);
+
+Console.WriteLine();
