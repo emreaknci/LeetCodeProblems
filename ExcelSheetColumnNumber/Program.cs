b00b08571f41b3b0691a94adc7d31acf0bb517fb
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-05-10 10:09:50 +0300

    Added ExcelSheetColumnNumber Problem

---
+++ ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Problem: https://leetcode.com/problems/excel-sheet-column-number/description/
+int TitleToNumber(string columnTitle)
+{
+    if (string.IsNullOrEmpty(columnTitle))
+        return 0;
+
+    int lastDigitValue = columnTitle[columnTitle.Length - 1] - 'A' + 1;
+
+    int remainingValue = TitleToNumber(columnTitle.Substring(0, columnTitle.Length - 1));
+
+    return remainingValue * 26 + lastDigitValue;
+}
+
+Console.WriteLine(TitleToNumber("ZA"));
