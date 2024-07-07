6c220af6ea13c6c5eeab9a3980e5f7cf798dea3b
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-07-07 19:17:42 +0300

    Added BestTimeToBuyAndSellStock Problem

---
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,23 @@
+﻿// Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+int MaxProfit(int[] prices)
+{
+    if (prices.Length < 2)
+        return 0;
+
+    int minPriceIndex = 0;
+    int maxProfit = 0;
+
+    for (int i = 1; i < prices.Length; i++)
+    {
+        if (prices[i] < prices[minPriceIndex])
+            minPriceIndex = i;
+
+        else if (prices[i] - prices[minPriceIndex] > maxProfit)
+            maxProfit = prices[i] - prices[minPriceIndex];
+    }
+
+    return maxProfit;
+}
+
+int[] prices = { 2,4,1 };
+Console.WriteLine(MaxProfit(prices));
