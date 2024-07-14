7c93965504b2457ec7db5f3d5b45653ceeed5507
Yunus Emre Akıncı <emreakinci696@gmail.com>
2024-07-14 16:44:56 +0300

    Added Valid Palindrome Problem

---
+++ ValidPalindrome/CustomStringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidPalindrome
+{
+    public static class CustomStringExtension
+    {
+        public static string RemoveSpecialCharacters(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in str)
+                if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                    sb.Append(c);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem: https://leetcode.com/problems/valid-palindrome/description/
+using ValidPalindrome;
+
+bool IsPalindrome(string s)
+{
+    s = s.RemoveSpecialCharacters().ToLower();
+    int leftIndex = 0;
+    int rightIndex = s.Length - 1;
+
+    while (leftIndex < rightIndex)
+    {
+        if (s[leftIndex] != s[rightIndex])
+            return false;
+
+        leftIndex++;
+        rightIndex--;
+    }
+
+    return true;
+
+}
+
+
+var str = "A man, a plan, a canal: Panama";
+Console.WriteLine(IsPalindrome(str));
+
+Console.ReadLine();
